1ffd1d323abb9209c26bb61c0c41a1235c662cc7
Daniel Bailey Smith <danielbaileysmith@gmail.com>
2020-02-23 14:59:46 -0700

    Hash function + cleaned up code

--- internship.py
+++ internship.py
@@ -1,23 +1,28 @@
-# Daniel Bailey Smith -- 2/16/2020
+# Daniel Bailey Smith -- 2/23/2020
 
 data_file = 'example_data.clp'
+df = data_file
 
 taxid_analysis = {}
-with open(data_file, 'r') as taxidf:
-    for line in taxidf:
-        key = tuple(line.strip().split(":")[-1].split(","))
-        if len(key) >= 50:
+with open(df, 'r') as df:
+    for line in df:
+        key_raw = tuple(line.strip().split(":")[-1].split(","))
+        if len(key_raw) >= 50:
             continue
-        if key in taxid_analysis:
-            taxid_analysis[key] += 1 #hashfunction wrap around this before += 1
         else:
-            taxid_analysis[key] = 1
-            #append a key value to a set
-            #cat each output and combine them together
-            #run hash on all them to reverse
+            key = hash(key_raw)
+            if key in taxid_analysis:
+                taxid_analysis[key] += 1
+            else:
+                taxid_analysis[key] = 1
+
+
+#append a key value to a set
+#cat each output and combine them together
+#run hash on all them to reverse
 
 # hash functions? how to import/use
 # figure out default dict
-# comment to check sync!!
 print(list(taxid_analysis.values()))
-
+print(list(taxid_analysis.keys()))
+print(taxid_analysis)
